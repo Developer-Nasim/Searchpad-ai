791bb9c16164145ed721989269b24e18d7bc97d9
Developer-Nasim <ajnasim72@gmail.com>
2024-10-11 22:08:55 +0600

    finished

---
+++ assets/js/main.js
@@ -0,0 +1,118 @@
+(function($) {
+  "use strict";
+  
+ // menu 
+ $('.siteBar-btn').click( function (event){ 
+    event.preventDefault()
+    $(this).toggleClass('active');   
+    $('.body_right').toggleClass('show');   
+  }); 
+  
+ // menu 
+ $('.back__btn').click( function (event){ 
+    event.preventDefault()
+    $(this).toggleClass('active');   
+    $('.side__bar').toggleClass('show');   
+  }); 
+
+  $('select').niceSelect();
+
+  // owlCarousel
+  $(".Video__slider").owlCarousel({
+    loop: true,
+    margin: 10,
+    items: 6,
+    navText: [
+      '<i class="fa fa-angle-left"></i>',
+      '<i class="fa fa-angle-right"></i>'
+    ],
+    nav: false,
+    dots: false,
+    responsive: {
+      0: {
+        items: 2
+      },
+      767: {
+        items: 3
+      },
+      992: {
+        items: 4
+      },
+      1200: {
+        items: 6
+      }
+    }
+  });
+
+  // owlCarousel
+  $(".brand-active").owlCarousel({
+    loop: true,
+    margin: 30,
+    items: 6,
+    navText: [
+      '<i class="fa fa-angle-left"></i>',
+      '<i class="fa fa-angle-right"></i>'
+    ],
+    nav: false,
+    dots: false,
+    responsive: {
+      0: {
+        items: 2
+      },
+      767: {
+        items: 3
+      },
+      992: {
+        items: 6
+      }
+    }
+  });
+
+  // Generate Video Button Selecting
+  var gbBtn = document.querySelectorAll(".gb__link a")
+
+  gbBtn.forEach(gbBtns => {
+    gbBtns.addEventListener("click", (e) => {
+      e.preventDefault()
+      gbBtnremove()
+      gbBtns.classList.add("active")
+    })
+  })
+
+  function gbBtnremove() {
+    gbBtn.forEach(gbBtns => {
+      gbBtns.classList.remove("active")
+    })
+  }
+
+  // Audio Plyer Active
+  var audioBtn = document.querySelectorAll(".audio__blk")
+
+  audioBtn.forEach(audioBtns => {
+    audioBtns.addEventListener("click", (e) => {
+      e.preventDefault()
+      audioBtnremove()
+      audioBtns.classList.add("active")
+    })
+  })
+
+  function audioBtnremove() {
+    audioBtn.forEach(audioBtns => {
+      audioBtns.classList.remove("active")
+    })
+  }
+
+
+  // page Animation
+  // AOS.init({
+  //   mirror: true,
+  //   duration: 1500,
+  //   initClassName: 'aos-init',
+  //   once: true,
+  // });
+
+  // data-aos="fade-up" 
+  // data-aos-delay="300" 
+
+ 
+})(jQuery);
